301603ebb75d2f77f830ae5aa3dd73f4fde3a2f7
romuant <romuant1@gmail.com>
2022-03-08 13:42:33 -0300

    mais exercicios

---
+++ arrays.js
@@ -0,0 +1,9 @@
+let tasksList = ['Tomar café', 'Reunião', 'Brincar com o cachorro'];
+
+console.log(tasksList.length);
+tasksList.push("Almoço com chefe", "jantar com esposa");
+for(let i=0; i<tasksList.length;i+=1){
+    console.log(tasksList[i]);
+}
+
+
