77473ded4da1690bf56a2664f40377f97eba9818
romuant <romuant1@gmail.com>
2022-03-08 15:20:36 -0300

    ais array

---
+++ maisArrays.js
@@ -0,0 +1,4 @@
+let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+let somaNumbers = numbers.reduce((a,b) =>a+b)
+console.log(somaNumbers);
+console.log(numbers.reduce((a,b) =>a+b)/numbers.length)
