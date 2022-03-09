8c1203b8150e3648e425517f8bf3967d2bb1c2af
romuant <romuant1@gmail.com>
2022-03-09 19:21:49 -0300

    subindo alguns arquivos

---
+++ fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-2-javascript-array-e-loop-for/fatorial.js
@@ -0,0 +1,7 @@
+//fatorial
+var fatorial = 10;
+var resultado = fatorial;
+for (var i = 1; i < fatorial; i++) {
+    resultado = resultado *i;
+}
+console.log("resultado fatorial de 10 é = ",resultado)
---
+++ variaveln.js
@@ -0,0 +1,10 @@
+/*
+No exercício abaixo, já deixamos a lógica pronta.
+Utilizando essa lógica, qual foi o valor que salvo na variável N em que o resultado que será impresso pelo console.log dará 55?*/
+
+const n =9
+let resultado =55
+for(let i=0; i<=n; i+=1){
+    resultado -=i;
+}
+console.log(resultado);
